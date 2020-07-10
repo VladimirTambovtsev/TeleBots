8d8099765a68fa3104c233597fad7a094f01654b
Vladimir Tambovtsev <tambovcev99@mail.ru>
2020-07-11 01:42:08 +0300

    set up crypto bot; set up facts bot; set up media images bot

--- echo-bot/bot.js
+++ echo-bot/bot.js
@@ -3,21 +3,51 @@ const Telegraf = require('telegraf')
 const bot = new Telegraf('1333571130:AAHlfYrrvxsWFwGDckPsuHIZMRanET0wwkc')
 
 const helpMessage = `
+Say something to me
 /start - start the bot
 /help - command reference
+/echo - say "You said echo"
+/echo <msg> - echo a message
 `
 
-// built-in commands
+bot.use((ctx, next) => {
+  if (ctx.updateSubTypes[0] == 'text') {
+    bot.telegram.sendMessage(
+      -333181632,
+      ctx.from.username + ' said: ' + ctx.message.text
+    )
+  } else {
+    bot.telegram.sendMessage(
+      -333181632,
+      ctx.from.username + ' sent ' + ctx.updateSubTypes[0]
+    )
+  }
+  next()
+})
+
 bot.start(ctx => {
-  ctx.reply(`Hi, ${ctx.from.first_name}! I'm Echo Bot`)
+  ctx.reply('Hi I am Echo Bot')
   ctx.reply(helpMessage)
 })
 
-bot.help(ctx => ctx.reply(helpMessage))
+bot.help(ctx => {
+  ctx.reply(helpMessage)
+})
 
 bot.command('echo', ctx => {
-  let input = ctx.message.text
-  let inputArr = input.split(' ')
+  let input = ctx.message.text //get input from user
+  let inputArray = input.split(' ') //split input by spaces
+  let message = '' //create variable for message to output to user
+
+  if (inputArray.length == 1) {
+    //check if array just contains "/echo"
+    message = 'You said echo'
+  } else {
+    inputArray.shift() //remove first element in array - "/echo"
+    message = inputArray.join(' ') //join all elements into a string separated by spaces
+  }
+
+  ctx.reply(message) //reply message to user
 })
 
 bot.launch()
